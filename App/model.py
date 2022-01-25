330b2d22e3552ab81624a99b223b7893dde5ea93
K-logeshwaran <87173747+K-logeshwaran@users.noreply.github.com>
2022-01-25 14:51:00 +0530

    Update model.py

--- App/model.py
+++ App/model.py
@@ -1,12 +1,12 @@
-# from pydantic import ColorError
-# from sqlalchemy import Boolean, Column, Integer, String
-# from sqlalchemy.orm import relationship
+from pydantic import ColorError
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
 
-# from .database import Base
+from .database import Base
 
-# class User(Base):
-#     __tablename__="posts"
-
-#     id = Column(Integer,primary_key=True)
-#     posts=Column(String,nullable=False)
-#     title=Column(String,nullable=False)
+class User(Base):
+     __tablename__="posts"
+     id = Column(Integer,primary_key=True)
+     posts=Column(String,nullable=False)
+     title=Column(String,nullable=False)
+     published=Column(name='published',type_=Boolean,server_default="true",nullable=False)
